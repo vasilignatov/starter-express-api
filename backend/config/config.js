f50ea7c8d4973cd74784cb737090c6483e000388
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-01-27 16:06:20 +0200

    Add MongoDB connection string

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     production: {
         PORT: process.env.PORT || 3000,
-        DB_CONNECTION_STRING: '',
+        DB_CONNECTION_STRING: 'mongodb+srv://js-quiz-app:Bz4w7hEwUHg0MgB0@cluster0.fq8kles.mongodb.net/?retryWrites=true&w=majority',
         JWT_SECRET: '$2y$10$xzlJAyCJAfoaBZIez.I0EO3KZAuXr66Voh/YmIQrGnjKUN3/WOJSq',
         TOKEN_COOKIE_NAME: 'quiz_token'
     }
